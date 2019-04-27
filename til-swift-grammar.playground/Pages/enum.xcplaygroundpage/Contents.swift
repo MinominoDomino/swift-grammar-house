5b937964cf0843059f0c2498ce60f159fff5a4bc
Minho Kim <nobility7192@gmail.com>
2019-04-27 17:49:46 +0900

    docs enum

--- til-swift-grammar.playground/Pages/enum.xcplaygroundpage/Contents.swift
+++ til-swift-grammar.playground/Pages/enum.xcplaygroundpage/Contents.swift
@@ -4,93 +4,89 @@ import Foundation
  키워드는 `enum`를 사용해요
  열거형은 다른 타입들과 다르게 추가/수정이 동적으로 불가능해요.
  소스에 선언된 상태로 사용된답니다.
-
- 그리고 배열은 데이터의 순서를 보장하지만 세트에서는 데이터의 순서가 랜덤해요.
- 예를들어 배열[1, 2, 3]과 세트[1, 2, 3]이 있을때 새로운 값인 4와 5를 추가하면
- 배열은 [1, 2, 3, 4, 5]이지만, 세트는 [2, 3, 1, 4, 5]이렇게 랜덤하게 구성됩니다!
- 
- 이러한 차이점 두개는 세트에서 가장 중요한 내용이에요.
- 
- https://developer.apple.com/documentation/swift/set
+ 기존 C에서 사용하던 열거형과는 다르게 정수형이 아닌 String, Char, Float 등 다른 값들을 사용할 수 있어요
+ https://docs.swift.org/swift-book/LanguageGuide/Enumerations.html
  */
 
-/* 세트 선언 */
+/* 열거형 선언 */
 /*
- 인피니티스톤 세트를 만들어보겠습니다.
- 인피니티스톤은 딱 6개의 스톤으로 구성되어 있죠? 마블 좋아하시는 분들은 다 아실거에요ㅎㅎㅎㅎㅎ
- 그리고 각 스톤은 무조건 하나만 있죠!! 그럼 세트를 배우는 예제로 딱인 것 같네요!
+ 상,하,좌,우로 움직이는 케릭터의 움직임을 표현하는 열거형 타입을 만들어볼게요.
  어서 만들어봅시다.
  */
 
-//var infinityStone: Set<String> = Set<String>()
-var infinityStone: Set<String> = ["Reality", "Soul", "Time"]    //3개의 스톤을 모았어요.
-
-/* 세트 메서드 */
-// -`isEmpty`는 세트가 비어 있는지 확인합니다.
-print(infinityStone.isEmpty)
-
-// -`count`는 세트의 개수를 알려줍니다.
-print(infinityStone.count)
-
-// -`contains`는 특정 값이 있는지 확인합니다.
-print(infinityStone.contains("Soul"))
-
-// -`insert(_:)`는 세트에 값을 추가합니다.
-infinityStone.insert("Mind")
-print(infinityStone)
-
-// -`remove(_:)`는 세트에서 값을 삭제합니다.
-infinityStone.insert("Time")
-print(infinityStone)
-
-// -`remove(_:)`는 세트에서 값을 삭제합니다.
-infinityStone.removeFirst()
-print(infinityStone)
-
-// -`popFirst()`는 세트에서 값을 삭제합니다.
-// removeFirst()와 다른점은 세트가 비어있을때 removeFirst는 에러이지만 popFirst는 nil을 반환합니다.
-infinityStone.removeAll()
-infinityStone.popFirst()
-print(infinityStone)
-
-/* 세트의 특징 */
-var arrayData: [Int] = [1, 2, 3]
-var setData: Set<Int> = [1, 2, 3]
-for number in [4, 5, 2] {
-    arrayData.append(number)
-    setData.insert(number)
+enum MoveInfoEx {
+    case up
+    case down
+    case left
+    case right
 }
-print("array is \(arrayData)")
-print("set is \(setData)")
-//위 코드를 돌려보면 배열은 값도 순서대로 들어있고 갯수도 6개들어있지만, 세트는 순서도 뒤죽박죽 갯수도 2라는 값이 중복되어서 5개만 있죠?
 
-/* 세트의 활용 */
-//세트는 교집합, 합집합, 여집합, 차집합 등 집합 계산에 유용하게 쓸 수 있어요!
-let myClassTable: Set<String> = ["kor", "eng", "math", "cs"]
-let girlFriendClassTable: Set<String> = ["kor", "art", "music", "cs"]
+enum MoveInfo { case up, down, left, right}    // case를 하나만 쓰고 ,로 구분하여 한번에 표현
 
-//교집합
-var commonClass: Set<String> = myClassTable.intersection(girlFriendClassTable)
-print(commonClass)
+/*
+ 위 처럼 MoveInfo라는 하나의 사용자 타입을 만들수있어요.
+ 그럼 사용자 움직임을 가지는 변수를 MoveInfo타입으로 만들어봅시다.
+ */
 
-//합집합
-var totalClass: Set<String> = myClassTable.union(girlFriendClassTable)
-print(totalClass)
+var playerMove: MoveInfo
 
-//여집합
-var notCommonClass: Set<String> = myClassTable.symmetricDifference(girlFriendClassTable)
-print(notCommonClass)
+/* 열거형 활용 */
+/*
+ 케릭터의 움직임을 지니는 MoveInfo타입과 이 타입으로 만들어진 변수 playerMove를 가지고 실제 우리가 케릭터를 움직였을때!
+ 케릭터가 어디로 움직였는지 알아야겠죠?
+ 열거형은 보통 `switch case`와 같이 활용됩니다.
+ 예제를 볼게요!
+ */
+playerMove = .left  //왼쪽으로 움직임
+switch playerMove {
+case .up:
+    print("player is move to up")
+case .down:
+    print("player is move to down")
+case .left:
+    print("player is move to left")     //출력
+case .right:
+    print("player is move to right")
+}
 
-//차집합
-var girFriendNotHaveClass: Set<String> = myClassTable.subtracting(girlFriendClassTable)
-print(girFriendNotHaveClass)
+/* 열거형 특징 */
+/*
+ - 원시 값(Raw Values)
+ 각 열거형의 항목들은 하나의 값이지만 각 항목들이 `원시 값`을 가질 수 있습니다.
+ "무슨말인가요...?"
+ 직접 보고 느끼는게 빠를거에요
+ 숫자를 말하면 무슨 숫자인지 알려주는 예제를 만들어 볼게요.
+ */
+
+enum NumberSound: Int {
+    case one = 1, two = 2, three = 3
+}
+
+var number: NumberSound = .two
+print(number.rawValue)  //2
 
 
+/*
+ - 연관 값(Associated Values)
+ 각 case 항목이 자신과 연관된 값을 가질 수 있습니다.
+ 항목뒤에 소괄호로 표현하고 custom type의 추가적인 정보를 저장하는 거죠
+ 
+ kiosk를 주문 받는 예제를 만들어 볼게요.
+ 먼저, kiosk타입으로 enum를 선언하고 각 항목은 브랜드명으로 하고 **브랜드마다 설치 위치와 갯수 그리고 버전**을 받겠습니다.
+ */
+enum kiosk {
+    case macdor(area: String, count: Int, version: Float)
+    case starbuk(String, Int, Float)
+}
 
+// 키오스크를 주문할게요 **맥도르 서울지점에 1.0버전 3대를 주문**해봅시다.
+var kioskOrder: kiosk = .macdor(area:"Seoul", count:3, version: 1.0)
 
-
-
-
-
-
+// 주문을 확인해봅시다.
+switch kioskOrder {
+case .macdor(let area, let count, let version):
+    print("macdor : \(area)Store, \(count)EA, \(version)Type")
+case .starbuk(let area, let count, let version):
+    print("starbuk : \(area)Store, \(count)EA, \(version)Type")
+}
 
