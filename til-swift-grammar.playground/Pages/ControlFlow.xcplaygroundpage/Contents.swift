d2776254bfedb580586ecc0330e8a404b8b08faf
Minho Kim <nobility7192@gmail.com>
2019-04-29 01:34:31 +0900

    docs controlFlow

--- til-swift-grammar.playground/Pages/ControlFlow.xcplaygroundpage/Contents.swift
+++ til-swift-grammar.playground/Pages/ControlFlow.xcplaygroundpage/Contents.swift
@@ -1,5 +1,29 @@
 import Foundation
 
+/* control flow
+ 제어문은 특정 조건에 따라 기능을 제어하는 기능입니다.
+ 제어문에는 `for-in`, `while`, `switch`, `if guard`가 있습니다.
+ 하나씩 알아봅시다.
+ [Apple swift document - Control Flow](https://docs.swift.org/swift-book/LanguageGuide/ControlFlow.html)
+ */
+
+/* for-in 루프 */
+/*
+ 가장 기본적으로 배열에 담긴 자료를 순차적으로 순회를 합니다.
+ - 원형 표현 `for [임시변수명] In [배열명] { }`
+ */
+
+// "KIM", "LEE", "PARK"이 들어있는 배열 names입니다.
+var names: [String] = ["KIM", "LEE", "PARK"]
+
+for name in names {
+    print("name is \(name)")
+}
+//name is KIM
+//name is LEE
+//name is PARK
+
+// 이번엔 딕셔너리에 담긴 자료를 순차적으로 순회합니다.
 // ["KIM":10, "LEE":20, "PARK":30]이 들어있는 딕셔너리 타입의 personInfo입니다.
 var personInfo: [String: Int] = ["KIM":10, "LEE":20, "PARK":30]
 
@@ -9,3 +33,59 @@ for (name, age) in personInfo {
 //name is KIM and age is 10
 //name is LEE and age is 20
 //name is PARK and age is 30
+
+// 숫자 범위를 정해서 순회를 합니다.
+// for [임시변수] in [from...to]
+for number in 5...10 {
+    print(number)   //5 6 7 8 9 10
+}
+
+// 범위 연사자를 사용해서 순회를 합니다.
+var clock: Int = 13
+for time in 1..<clock {
+    print(time) //1 2 3 4 5 6 7 8 9 10 11 12
+}
+
+// stride(from:to:through) 함수를 사용해서 순회를 할 수 있습니다.
+// stride함수는 from값에서 to값까지 through만큼 떨어지면서 시퀀스를 만드는 함수입니다.
+for value in stride(from: 0, through: 6, by: 2) {
+    print(value)    //0 2 4 6
+}
+
+// 값 제어없이 단순반복경우, 임시변수를 `_`로 생략할 수 있고 성능을 더 높일 수 있습니다.
+let base = 3
+let power = 10
+var answer = 1
+for _ in 1...power {
+    answer *= base
+}
+print("\(base) to the power of \(power) is \(answer)")  //3 to the power of 10 is 59049
+
+/* while 루프 */
+/*
+ while은 특정 조건이 만족하는 동안 루프를 실행하고 조건이 만족되지 않으면 루프를 빠져나와요.
+ 기본 while과 repeat-while 두 종류의 루프가 있습니다.
+ repeat-while은 기본 while과 다르게 먼저 루프를 실행하고 조건 검사가 이루워집니다.
+ 가장 기본적인 예제를 해볼게요.
+ - 원형 표현 `while [조건] { }`
+ - 원형 표현 `repeat { } while [조건]
+ */
+
+var startTime: Int = 0
+var endTime: Int = 10
+
+while startTime < endTime {
+    startTime += 1
+}
+print("startTime is \(startTime)")  //startTime is 10
+
+
+repeat {
+    startTime += 1
+} while startTime < endTime
+print("startTime is \(startTime)")  //startTime is 11
+// repeat-while은 무조건 루프를 먼저 실행하고 검사를 하게되어 startTime이 11로 증가했어요.
+
+
+
+
